01421db091d8ed23d79b88da0661d1ca966dfd77
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2018-11-29 22:42:48 -0500

    Handle edge cases for palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,19 @@
 def is_palindrome(word):
     """Check if a given word is a palindrome."""
+
+    if not isinstance(word, str):
+        raise ValueError('Word must be a string')
+
     n = len(word)
+
+    # Edge cases
+    if n == 0:
+        raise ValueError('I still need convincing that empty string '
+                         'is a palindrome, Dan')
+    elif n == 1:
+        return True
+
+    word = word.lower()
     i_mid = n // 2
 
     # Skip middle letter if odd.
@@ -15,14 +28,27 @@ def is_palindrome(word):
     return first_half == second_half
 
 
-# NOTE: Could use pytest.mark.parametrize here but I don't want
-#       that dependency.
+# NOTE: Could use pytest here but I don't want that dependency.
 def test_palindrome():
-    words1 = ['tacocat', 'abba']
-    words2 = ['kebab', 'babb']
+    words1 = ['a', 'GG', 'gig', 'tacocat', 'abbA']
+    words2 = ['go', 'goo', 'kebab', 'babB']
 
     for w in words1:
         assert is_palindrome(w)
 
     for w in words2:
         assert not is_palindrome(w)
+
+    try:
+        is_palindrome('')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError('Empty string did not raise ValueError')
+
+    try:
+        is_palindrome(1.0)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError('Non-string did not raise ValueError')
