14a5482bdfbc82f599303c010f0a6eac119b2529
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-10-16 15:11:55 -0400

    Fix doc in tribute_finder

--- tribute_finder.py
+++ tribute_finder.py
@@ -4,7 +4,7 @@ import random
 from collections import Counter
 
 
-def pick_tribute(population, previous_tribute, seed=None):
+def pick_tribute(population, previous_tributes, seed=None):
     """Pick a tribute. May the odds be ever in your favor.
 
     Parameters
@@ -27,7 +27,7 @@ def pick_tribute(population, previous_tribute, seed=None):
 
     """
     population = list(set(population))  # Ensure uniqueness
-    c = Counter(previous_tribute)
+    c = Counter(previous_tributes)
     max_weight = len(population)
 
     if max_weight == 0:
@@ -47,9 +47,9 @@ def test_pick_tribute():
     population = ['Robb Stark', 'Jon Snow', 'Sansa Stark', 'Arya Stark',
                   'Bran Stark', 'Rickon Stark', 'Theon Greyjoy']
     # Arya Stark should be the least likely, followed by Robb Stark,
-    # then Jon/Theon. The rest is fair game.
-    previous_tribute = ['Robb Stark', 'Jon Snow', 'Robb Stark',
-                        'Arya Stark', 'Theon Greyjoy', 'Arya Stark',
-                        'Arya Stark', 'Dolores']
-    assert pick_tribute(population, previous_tribute, seed=1234) == 'Sansa Stark'  # noqa
-    assert pick_tribute(population, previous_tribute, seed=4321) == 'Bran Stark'  # noqa
+    # then Jon/Theon. Dolores left. The rest is fair game.
+    previous_tributes = ['Robb Stark', 'Jon Snow', 'Robb Stark',
+                         'Arya Stark', 'Theon Greyjoy', 'Arya Stark',
+                         'Arya Stark', 'Dolores']
+    assert pick_tribute(population, previous_tributes, seed=1234) == 'Sansa Stark'  # noqa
+    assert pick_tribute(population, previous_tributes, seed=4321) == 'Bran Stark'  # noqa
